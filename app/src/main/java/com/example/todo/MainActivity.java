1bd005580d4e9669e32255e6e113da6e0522111b
swppppp <sw931110@naver.com>
2019-04-24 10:40:51 +0900

    mainActivity java파일 작성

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,13 +1,94 @@
 package com.example.todo;
 
+import android.content.DialogInterface;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    DBHelper dbHelper;
+    ArrayAdapter<String> mAdapter;
+    ListView lsTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new DBHelper(this);
+
+        lsTask = findViewById(R.id.lsTask);
+        loadTaskList();
+    }
+
+    // task목록 업뎃
+    private void loadTaskList() {
+        ArrayList<String> taskList = dbHelper.getTaskList();
+        if(mAdapter==null) {
+            mAdapter = new ArrayAdapter<String>(this, R.layout.row, R.id.task_title, taskList);
+            lsTask.setAdapter(mAdapter);
+        }else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        // change menu icon color
+        Drawable icon = menu.getItem(0).getIcon();
+        icon.mutate();
+        icon.setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add_task:
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this).
+                        setTitle("새로운 할 일을 추가하세요:D").
+                        setMessage("무슨 일을 할 것인가요?").
+                        setView(taskEditText).
+                        setPositiveButton("추가", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                dbHelper.insertTask(task);
+                                loadTaskList();
+                            }
+                        }).
+                        setNegativeButton("취소", null).
+                        create();
+                dialog.show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deleteTask(View view) {
+        View parent = (View) view.getParent();
+        TextView taskTextView = findViewById(R.id.task_title);
+        String task = String.valueOf(taskTextView.getText());
+        dbHelper.deleteTask(task);
+        loadTaskList();
     }
 }
